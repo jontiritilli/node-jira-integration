0b1eaaf9fc37c4c96216ede15cf610dfd1cc1b29
Jonathan <jonathantiritilli@gmail.com>
2019-07-21 10:15:49 -0700

    basic html and routes

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Jira Rest Home' });
 })
 
-router.get('/getAllBoards', (req, res, next) => {
-  getAllBoards()
-    .then(response => {
-      res.send(JSON.stringify(response));
-    })
-    .catch(error => {
-      next(error)
-    })
+router.get('/getBoard', async (req, res, next) => {
+  if(!req.query.id) {
+    res.send('Please provide an board number');
+  }
+  try {
+    board = await getBoard(req.query.id);
+    res.render('board', { title: board.name, boards: JSON.stringify(board) })
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.get('/getAllBoards', async (req, res, next) => {
+  try {
+    const { values } = await getAllBoards()
+    console.log(values)
+    res.render('boards', { title: 'Board List', boards: JSON.stringify(values) })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.get('/findIssue', (req, res, next) => {
@@ -29,10 +41,21 @@ router.get('/findIssue', (req, res, next) => {
     })
 })
 
+const getBoard = async id => {
+  console.info('getBoard', 'Starting retrieve')
+  try {
+    const board = await Jira.getBoard(id);
+    console.info('getBoard', 'success', board)
+    return board;
+  } catch (e) {
+    return e;
+  }
+}
 const getAllBoards = async () => {
   console.info('getAllBoards', 'Starting retrieve')
   try {
     const boards = await Jira.getAllBoards();
+    console.info('getAllBoards', 'success')
     return boards;
   } catch (e) {
     return e;
@@ -43,6 +66,7 @@ const findIssue = async issueNumber => {
   console.info('findIssue', 'Starting retrieve');
   try {
     const issue = await Jira.findIssue(issueNumber);
+    console.info('findIssue', 'success')
     return issue;
   } catch (e) {
     return e;
